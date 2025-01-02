e463bb26e44cd53e546353bb5801a37d381d1d6a
srinrao23583 <srinivas.raok@yahoo.com>
2025-01-02 14:56:02 -0500

    changes

--- src/main/java/com/nfcu/alertmedia/poc/AlertmediaPocApplication.java
+++ src/main/java/com/nfcu/alertmedia/poc/AlertmediaPocApplication.java
@@ -1,5 +1,10 @@
 package com.nfcu.alertmedia.poc;
 
+import java.io.File;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +20,16 @@ public class AlertmediaPocApplication {
 	
 	@GetMapping("/notification")
 	public String notification() {
+		//File file = new File("resources/chrome.exe");
+		//String absolutePath = file.getPath();
+		System.setProperty("webdriver.chrome.driver", AlertmediaPocApplication.class.getClassLoader().getResource("chrome.exe").getPath());
+		// Define ChromeDriver options
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        
+		WebDriver driver= new ChromeDriver(options);
+
+		driver.get("https://www.browserstack.com/");
 		return " AlertMedia Notofication is received!!!";
 	}
 
