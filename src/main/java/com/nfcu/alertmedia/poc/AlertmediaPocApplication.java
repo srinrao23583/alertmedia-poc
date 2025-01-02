8b0469c12ae2e0a975e2addd8f30615677b84b06
srinrao23583 <srinivas.raok@yahoo.com>
2025-01-02 17:35:22 -0500

    playright

--- src/main/java/com/nfcu/alertmedia/poc/AlertmediaPocApplication.java
+++ src/main/java/com/nfcu/alertmedia/poc/AlertmediaPocApplication.java
@@ -1,14 +1,14 @@
 package com.nfcu.alertmedia.poc;
 
-import java.io.File;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
 
 @SpringBootApplication
 @RestController
@@ -20,9 +20,10 @@ public class AlertmediaPocApplication {
 	
 	@GetMapping("/notification")
 	public String notification() {
+		/*
 		//File file = new File("resources/chrome.exe");
 		//String absolutePath = file.getPath();
-		System.setProperty("webdriver.chrome.driver", AlertmediaPocApplication.class.getClassLoader().getResource("chrome.exe").getPath());
+		//System.setProperty("webdriver.chrome.driver", AlertmediaPocApplication.class.getClassLoader().getResource("chrome.exe").getPath());
 		// Define ChromeDriver options
         ChromeOptions options = new ChromeOptions();
         options.addArguments("headless");
@@ -30,6 +31,22 @@ public class AlertmediaPocApplication {
 		WebDriver driver= new ChromeDriver(options);
 
 		driver.get("https://www.browserstack.com/");
+		*/
+		
+		try (Playwright playwright = Playwright.create()) {
+            BrowserType chromium = playwright.chromium();
+
+            // Connect to Browserless 
+            Browser browser = chromium.connectOverCDP("wss://chrome.browserless.io?token=RVyoFsgeAqd7dd210b99202d220f5e66fa5724ac81");
+
+            // Create a new page and navigate to a URL
+            Page page = browser.newPage();
+            page.navigate("https://www.google.com/");
+            
+
+            browser.close();
+        }
+		
 		return " AlertMedia Notofication is received!!!";
 	}
 
