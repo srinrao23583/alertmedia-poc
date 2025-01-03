5a89ea5114069a78a19f6a9685f85e35141a549e
srinrao23583 <srinivas.raok@yahoo.com>
2025-01-03 10:51:58 -0500

    changes jan 3

--- src/main/java/com/nfcu/alertmedia/poc/AlertmediaPocApplication.java
+++ src/main/java/com/nfcu/alertmedia/poc/AlertmediaPocApplication.java
@@ -1,53 +1,62 @@
 package com.nfcu.alertmedia.poc;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserType;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
 
 @SpringBootApplication
 @RestController
 public class AlertmediaPocApplication {
 
 	public static void main(String[] args) {
+		//String myPath = AlertmediaPocApplication.class.getClassLoader().getResource("geckodriver.exe").getPath();
+		//System.out.println("myPath1:" + myPath);
+		//String myPath1 = myPath.replaceAll("/C:", "");
+		//System.out.println("myPath new:" + myPath1);
 		SpringApplication.run(AlertmediaPocApplication.class, args);
 	}
 	
 	@GetMapping("/notification")
 	public String notification() {
-		/*
-		//File file = new File("resources/chrome.exe");
-		//String absolutePath = file.getPath();
-		//System.setProperty("webdriver.chrome.driver", AlertmediaPocApplication.class.getClassLoader().getResource("chrome.exe").getPath());
-		// Define ChromeDriver options
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("headless");
-        
-		WebDriver driver= new ChromeDriver(options);
-
-		driver.get("https://www.browserstack.com/");
-		*/
-		
-		try (Playwright playwright = Playwright.create()) {
-            BrowserType chromium = playwright.chromium();
-
-            // Connect to Browserless 
-            Browser browser = chromium.connectOverCDP("wss://chrome.browserless.io?token=RVyoFsgeAqd7dd210b99202d220f5e66fa5724ac81");
-
-            // Create a new page and navigate to a URL
-            Page page = browser.newPage();
-            page.navigate("https://www.google.com/");
-            
-
-            browser.close();
-        }
-		
-		return " AlertMedia Notofication is received!!!";
+		try {
+			//File file = new File("resources/chrome.exe");
+			//String absolutePath = file.getPath();
+			//System.setProperty("webdriver.chrome.driver", AlertmediaPocApplication.class.getClassLoader().getResource("chrome.exe").getPath());
+			String myPath = getClass().getClassLoader().getResource("chromedriver.exe").getPath();
+			//String myPath = getClass().getClassLoader().getResource("geckodriver.exe").getPath();
+			System.out.println("myPath2:" + myPath);
+			System.setProperty("webdriver.chrome.driver", myPath.replaceAll("/C:", ""));
+			// Define ChromeDriver options
+	        ChromeOptions options = new ChromeOptions();
+	        options.addArguments("headless");
+			
+			WebDriver driver= new ChromeDriver();
+	
+			driver.get("https://www.browserstack.com/");
+			
+			/*
+			try (Playwright playwright = Playwright.create()) {
+	            BrowserType chromium = playwright.chromium();
+	
+	            // Connect to Browserless 
+	            Browser browser = chromium.connectOverCDP("wss://chrome.browserless.io?token=RVyoFsgeAqd7dd210b99202d220f5e66fa5724ac81");
+	
+	            // Create a new page and navigate to a URL
+	            Page page = browser.newPage();
+	            page.navigate("https://www.google.com/");
+	            
+	
+	            browser.close();
+	        }
+			*/
+		} catch (Exception e) {
+			return "AlertMedia processing is failed:" + e.getMessage();
+		}
+		return "AlertMedia Notofication is received!!!";
 	}
 
 }
